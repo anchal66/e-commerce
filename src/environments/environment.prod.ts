914dd2800c5bb530940235f8a1e3b362c5746070
Avinash Anchal <you@example.comanchalavinash66@gmail.com>
2020-01-15 07:55:20 +0530

    basic front end part

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,13 @@
 export const environment = {
-  production: true
+  production: true,
+  firebase:{
+    apiKey: 'AIzaSyC7MPvg9Ot-_Sfi_IsrU3fc3K0HnpdPTVk',
+    authDomain: 'oshop-aea00.firebaseapp.com',
+    databaseURL: 'https://oshop-aea00.firebaseio.com',
+    projectId: 'oshop-aea00',
+    storageBucket: 'oshop-aea00.appspot.com',
+    messagingSenderId: '100140562511',
+    appId: '1:100140562511:web:14adf86ef002c51083523a',
+    measurementId: 'G-11YVBSNDBH'
+  }
 };
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,7 +3,17 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false
+  production: false,
+  firebase:{
+    apiKey: 'AIzaSyC7MPvg9Ot-_Sfi_IsrU3fc3K0HnpdPTVk',
+    authDomain: 'oshop-aea00.firebaseapp.com',
+    databaseURL: 'https://oshop-aea00.firebaseio.com',
+    projectId: 'oshop-aea00',
+    storageBucket: 'oshop-aea00.appspot.com',
+    messagingSenderId: '100140562511',
+    appId: '1:100140562511:web:14adf86ef002c51083523a',
+    measurementId: 'G-11YVBSNDBH'
+  }
 };
 
 /*
