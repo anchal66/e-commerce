8bfcb3ac7503892091e2a76db30e8c74bd13a660
Avinash Anchal <you@example.comanchalavinash66@gmail.com>
2020-01-15 08:11:02 +0530

    Implementing Login and Logout from firebase

--- src/app/navbar/navbar.component.ts
+++ src/app/navbar/navbar.component.ts
@@ -1,15 +1,16 @@
 import { Component, OnInit } from '@angular/core';
+import { AngularFireAuth } from 'angularfire2/auth';
 
 @Component({
   selector: 'app-navbar',
   templateUrl: './navbar.component.html',
   styleUrls: ['./navbar.component.css']
 })
-export class NavbarComponent implements OnInit {
+export class NavbarComponent {
 
-  constructor() { }
+  constructor(private afAuth: AngularFireAuth) { }
 
-  ngOnInit() {
+  logout(){
+    this.afAuth.auth.signOut();
   }
-
 }
